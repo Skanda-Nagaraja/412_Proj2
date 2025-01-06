c4c7945072d727263fa1fbe25f531115baaa6f52
Skanda-Nagaraja <s632003130@tamu.edu>
2025-01-06 09:19:26 +0800

    request

---
+++ RequestGenerator.cpp
@@ -0,0 +1,34 @@
+#include "RequestGenerator.h" 
+#include <iostream>
+#include <string>
+using namespace std;
+
+RequestGenerator::RequestGenerator() {
+    srand(time(NULL));
+}
+
+string genIp () {
+    string ip = "";
+    for (int i = 0; i < 4; i++) {
+        ip += to_string(rand() % 256);
+        if (i < 3) {
+            ip += ".";
+        }
+    }
+    return ip;
+}
+Request* RequestGenerator::generateRequest() {
+    Request* r = new Request();
+    r->ipIn = genIp();
+    r->ipOut = genIp();
+
+    r->time = (rand() % 1000) + 1;
+
+    if(rand() % 2 == 0) {
+        r->type = S;
+    } else {
+        r->type = P;
+    }
+    
+    return r;
+}
