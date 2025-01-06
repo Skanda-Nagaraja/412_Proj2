6a911ec62025852d5b2387b39f19b4e5ea2891ed
Thomas Ankner <tankner@tamu.edu>
2025-01-06 10:48:48 +0800

    added input request

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include "RequestGenerator.h"
 #include "LoadBalancer.h"
 #include "WebServer.h"
+#include <iostream>
+#include <sstream>
 
 
 
 int main() {
+    int initial_servers = 0;
+    int clock_cycles = 0;
+    std::string input;
+
+    std::cout << "Enter initial number of servers: ";
+    std::getline(std::cin, input);
+    std::stringstream(input) >> initial_servers;
+
+    std::cout << "Enter number of clock cycles: ";
+    std::getline(std::cin, input);
+    std::stringstream(input) >> clock_cycles;
+
+    std::cout << "Simulation started with " << initial_servers << " servers and " << clock_cycles << " clock cycles.\n";
     int initialCapacity = 10000;
     int numCycles = 10000;
     RequestGenerator rg;
